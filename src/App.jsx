9d2bfa712d9ffc0c0b3dc4dc3de1214196383314
Nate Laffan <nate.laffan@gmail.com>
2023-02-16 14:56:57 -0800

    Deploy scripts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { RigidBody, Physics, CuboidCollider } from '@react-three/rapier'
+import { RigidBody, Physics, CuboidCollider } from "@react-three/rapier";
 import Webcam from "react-webcam";
 import HandPoints from "./components/Handpoints";
 
@@ -21,21 +21,24 @@ const App = () => {
 
         <HandPoints webcamRef={webcamRef} />
         <Physics>
-          <RigidBody type="fixed"  restitution={1}  position={[0, - 1.25,0]  }>
+          <RigidBody type="fixed" restitution={1} position={[0, -1.25, 0]}>
             <mesh receiveShadow>
-                <boxGeometry args={ [ 10, 0.5, 10 ] } />
-                <meshStandardMaterial color="greenyellow" />
+              <boxGeometry args={[10, 0.5, 10]} />
+              <meshStandardMaterial color="greenyellow" />
             </mesh>
             <RigidBody type="fixed">
-              <CuboidCollider args={ [ 5, 2, 0.5 ] } position={ [ 0, 1, 5.5 ] } />
-              <CuboidCollider args={ [ 5, 2, 0.5 ] } position={ [ 0, 1, - 5.5 ] } />
-              <CuboidCollider args={ [ 0.5, 2, 5 ] } position={ [ 5.5, 1, 0 ] } />
-              <CuboidCollider args={ [ 0.5, 2, 5 ] } position={ [ - 5.5, 1, 0 ] } />
+              <CuboidCollider args={[5, 2, 0.5]} position={[0, 1, 5.5]} />
+              <CuboidCollider args={[5, 2, 0.5]} position={[0, 1, -5.5]} />
+              <CuboidCollider args={[0.5, 2, 5]} position={[5.5, 1, 0]} />
+              <CuboidCollider args={[0.5, 2, 5]} position={[-5.5, 1, 0]} />
+            </RigidBody>
           </RigidBody>
-
-
-          </RigidBody>
-          <RigidBody colliders="ball" restitution={1} position={ [ - 2, 2, 0 ] } scale={[.5,.5,.5]}>
+          <RigidBody
+            colliders="ball"
+            restitution={1}
+            position={[-2, 2, 0]}
+            scale={[0.5, 0.5, 0.5]}
+          >
             <mesh castShadow>
               <sphereGeometry />
               <meshStandardMaterial color="orange" />
@@ -43,18 +46,16 @@ const App = () => {
           </RigidBody>
 
           <RigidBody
-              position={ [ 0, - 0.8, 0 ] }
-              friction={ 0 }
-              type="kinematicPosition"
+            position={[0, -0.8, 0]}
+            friction={0}
+            type="kinematicPosition"
           >
-            <mesh castShadow scale={ [ 0.4, 2, 0.4 ] }>
-                <boxGeometry />
-                <meshStandardMaterial color="red" />
+            <mesh castShadow scale={[0.4, 2, 0.4]}>
+              <boxGeometry />
+              <meshStandardMaterial color="red" />
             </mesh>
-        </RigidBody>
-
+          </RigidBody>
         </Physics>
-        
 
         <OrbitControls />
       </Canvas>
