5f613fd760a0ee5a56f7f3478435e393958fe3b4
Samir Ghosh <hello@samir.tech>
2023-02-20 18:00:30 -0800

    optimization: less jumpiness and higher fps with truncate

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -11,19 +11,29 @@ function mapRange (value, a, b, c, d) {
     // then map it from (0..1) to (c..d) and return it
     return c + value * (d - c);
 }
+//stack overflow
+function truncate (number, digits) {
+  var multiplier = Math.pow(10, digits),
+      adjustedNum = number * multiplier,
+      truncatedNum = Math[adjustedNum < 0 ? 'ceil' : 'floor'](adjustedNum);
+
+  return truncatedNum / multiplier;
+};
 
 const Avatar = ({ webcamRef }) => {
-  const [fingerPosition, setFingerPosition] = useState([0,0,0]);
+  const [fingerPosition, setFingerPosition] = useState([0,1.5,0]);
   const vidSize = { width: 370 / 2, height: 280 / 2 };
+
 
   // Use a memo'd throttle to prevent state updates
   // from occuring each frame.
   // Samir wants to try skipping
   const throttledStateUpdate = useMemo(
     () =>
-      throttle((results) => {
-        setFingerPosition(results);
-      }, 100),
+      throttle((p) => {
+        console.log(p)
+        setFingerPosition([p[0],1,p[2]]);
+      }, 150),
     []
   );
 
@@ -39,26 +49,25 @@ const Avatar = ({ webcamRef }) => {
             // const raw = results[0].annotations.indexFinger[0] //this is the index finger
             const raw = results[0].landmarks[0]
 
-        //    throttledStateUpdate([
-        //         mapRange(raw[0],560,90,-5,5),
-        //         mapRange(raw[1],430,60,-5,5),
-        //         raw[2]]);
+        // //    throttledStateUpdate([
+        // //         mapRange(raw[0],560,90,-5,5),
+        // //         mapRange(raw[1],430,60,-5,5),
+        // //         raw[2]]);
 
            throttledStateUpdate(raw.map((p)=>{
-            return ((p/100) *-1)+2;
+            return ((truncate(p,2)/100) *-1)+2;
            }));
           
         }
       });
     };
-
     const handpose = ml5.handpose(webcamRef.current.video, modelLoaded);
   }, []);
   
 
   return (
     <RigidBody position={ fingerPosition} friction={0} type="kinematicPosition" restitution={1}>
-      <mesh scale={[0.4, 2, 0.4]}>
+      <mesh scale={[0.4, 3, 0.4]}>
         <boxGeometry />
         <meshStandardMaterial color="red" />
       </mesh>
